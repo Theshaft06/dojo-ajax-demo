537d1ca74d4d8e10b258fcf6d0bc6a2a6514d7ae
Eylem Ozaslan <eylem.ozaslan@gmail.com>
2014-03-12 15:40:28 -0700

    todos are now editable

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ end
 
 gem 'simple_form'
 
+gem 'best_in_place'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 class TodosController < ApplicationController
   
-  before_action :set_todos, only: [:index, :create, :destroy]
+  before_action :set_todos, only: [:index, :create, :destroy, :update]
 
   def index
   	@todo = Todo.new
@@ -8,16 +8,30 @@ class TodosController < ApplicationController
 
   def create
   	todo = Todo.create(todo_params)
-  	respond_to do |anything|
-  	  anything.js
+  	respond_to do |format|
+  	  format.js
   	end
   end
 
   def destroy
   	Todo.find(params[:id]).destroy
-  	respond_to do |derp|
-  	  derp.js
+  	respond_to do |format|
+  	  format.js
   	end
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update_attributes(todo_params)
+    respond_to do |format|
+      if todo.update_attributes(todo_params)
+        format.html { redirect_to todos_path }
+        format.json { head :ok }
+      else
+        format.html { render :action => "index" }
+        format.json { render :json => todo.errors.full_messages, :status => :unprocessable_entity }
+      end
+    end
   end
 
   private
